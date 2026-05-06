d268d64b36131f590c490b8c48facaeb2a023ce7
ssl-101 <inquiresiham@gmail.com>
2026-05-05 21:09:13 -0400

    area and  perimeter functions added

--- cpp-folder/src/main.cpp
+++ cpp-folder/src/main.cpp
@@ -7,16 +7,38 @@ using namespace std;
 void getLengthWidth (double& length, double& width)
 {
     do{
-        cout << "Enter length (greather than 0):" ;
+        cout << "Enter length (greater than 0):" ;
         cin >> length;
 
         if (length <=0) {
-            cout << "Error! Enter a number greater than 0. \n";
+            cout << " Error! Enter a number greater than 0: \n";
         }
 
-    } while (length <=0);//;;;
+    } while (length <=0);
+    do {
+        cout << " Enter width ( greater than 0 ): " ;
+         cin >> width ;
 
-    
+         if (width <=0) {
+            cout << "Error! Enter a number greater than 0 : \n";
+         }
+    } while (width <= 0);
+      
+}
+// calculation functions
+
+double calcPerimeter ( double length, double width)
+{
+    return 2 * (length + width);
+}
+double calcArea ( double length, double width)
+{
+    return length * width ;
+}
+void displayProperties (double perimeter, double area)
+{
+    cout<< " Perimeter: "  << perimeter << endl;
+    cout << " Area: " << area << endl;
 }
 int main()
 
