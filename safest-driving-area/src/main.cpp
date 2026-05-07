332f922759b9a15d1f07e7c8d016241ef9574f0d
ssl-101 <inquiresiham@gmail.com>
2026-05-07 15:01:43 -0400

    added function protocals

--- safest-driving-area/src/main.cpp
+++ safest-driving-area/src/main.cpp
@@ -1,9 +1,18 @@
+/*This program displays safest region
+based on accident amout , compared
+between two regions */
+
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main ()
-{
-    cout << "hello world"<<endl;
+//Functions for input , analyze input, display
+void getRegionInfo (string & regionName, int& accidents);
+// bool will determine true or false
+bool isLower (int value1, int value2);
+void showLowest ( string regionName, int accidents);
 
-    return 0;
+int main()
+{
+    
 }
